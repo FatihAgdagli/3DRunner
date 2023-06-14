b46349627b9a7524289ad61f950dee7005de1dc7
FatihAgdagli <agdaglifatih@gmail.com>
2023-06-14 08:36:18 +0300

    Input Controls Added

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,16 +5,46 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     [SerializeField] private float runningSpeed = 2f;
-    // Start is called before the first frame update
+    [SerializeField] private float xSpeed;
+    [SerializeField] private float limitX;
+    private float touchXDelta = 0f;
+    private float newX = 0f;
+    private float newZ = 0f;
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + runningSpeed * Time.deltaTime);
+        SwipeCheck();
+    }
+
+    private void SwipeCheck()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            touchXDelta = Input.GetTouch(0).deltaPosition.x / Screen.width;
+        }
+        else if(Input.GetMouseButton(0))
+        {
+            touchXDelta = Input.GetAxis("Mouse X");
+        }
+        else
+        {
+            touchXDelta = 0f;
+        }
+
+        newX = transform.position.x + xSpeed * touchXDelta * Time.deltaTime;
+        newX = Mathf.Clamp(newX, -limitX, limitX);
+
+        newZ = transform.position.z + runningSpeed * Time.deltaTime;
+
+        Vector3 newPosition = new Vector3(newX, transform.position.y, newZ);
+
         transform.position = newPosition;
     }
 }
