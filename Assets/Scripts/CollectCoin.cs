dddc63f25d3a8a8d939e00b8edde53807c6709d9
FatihAgdagli <agdaglifatih@gmail.com>
2023-06-14 19:15:48 +0300

    Obstacle1 Added

--- Assets/Scripts/CollectCoin.cs
+++ Assets/Scripts/CollectCoin.cs
@@ -1,14 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CollectCoin : MonoBehaviour
 {
+    [SerializeField] private int score;
+    [SerializeField] private TextMeshProUGUI scoreTMP;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Coin"))
         {
-            Debug.Log("Coin Collected...");
+            Destroy(other.gameObject);
+            AddScore();
         }
+    }
+
+
+    private void AddScore()
+    {
+        score++;
+        scoreTMP.text = $"Score: {score}";
     }
 }
